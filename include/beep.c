dffc934e4883313de4691100f88523f7446a4d6a
stanislaumich <staslz42stas@gmail.com>
2021-06-29 10:06:27 +0300

    Beep init added

---
+++ include/beep.c
@@ -0,0 +1,30 @@
+#define pinbeep 2
+
+void initbeep(void){
+ pinMode(pinbeep, OUTPUT);
+ digitalWrite(pinbeep, LOW);
+}
+void beep(int t1,int t2){
+  #ifdef pinbeep
+   unsigned long h;
+   h=millis();
+   while (millis()-h<(unsigned int)(t1)){
+    digitalWrite(pinbeep,HIGH);
+    delayMicroseconds(t2);
+    digitalWrite(pinbeep,LOW);
+    delayMicroseconds(t2);
+   }
+   digitalWrite(pinbeep,LOW);
+  #endif
+ }
+void shortbeep(void){
+  beep(125,150);
+ }
+void longbeep(void){
+  beep(250,250);
+ }
+void dshortbeep(void){
+  shortbeep();
+  delay(50);
+  shortbeep(); 
+ } 
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <beep.c>
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  initbeep();
+  dshortbeep();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
