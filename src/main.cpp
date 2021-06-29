17a073f9c79f721af6231060a76725e184c61933
stanislaumich <staslz42stas@gmail.com>
2021-06-29 11:23:03 +0300

    timers added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include <beep.cpp>
-#include<GyverUART.h>
+//#include<GyverUART.h>
+#include "GyverTimer.h"
  //  https://arduino-kit.ru/blogs/blog/project_33
  // https://microkontroller.ru/arduino-projects/gps-spidometr-na-arduino-i-oled-displee-svoimi-rukami/
- 
 
 
 
+GTimer myTimer1(MS, 60000);
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,5 +17,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if (myTimer1.isReady())
+    shortbeep();
+
 }
